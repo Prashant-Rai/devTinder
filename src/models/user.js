b7a4c3c018db11b1158eb3c20778556e4f98b0c9
prashant-rai <prashantrai117@gmail.com>
2025-10-08 02:20:31 +0530

    Added schema vlidation

--- src/models/user.js
+++ src/models/user.js
@@ -1,5 +1,5 @@
-const { type } = require("express/lib/response");
 const mongoose = require("mongoose");
+var validator = require("validator");
 
 const userSchema = new mongoose.Schema(
   {
@@ -9,11 +9,25 @@ const userSchema = new mongoose.Schema(
       required: [true, "First name is required field"],
       minLength: [3, "First name must be at least 3 characters"],
       maxLength: [30, "First name must be at most 30 characters"],
+      validate: {
+        validator: (value) => {
+          return validator.isAlpha(value);
+        },
+        message: "First name must contain only alphabetical characters",
+      },
     },
     lastName: {
       type: String,
       trim: true,
       required: [true, "Last name is required field"],
+      minLength: [3, "Last name must be at least 3 characters"],
+      maxLength: [30, "Last name must be at most 30 characters"],
+      validate: {
+        validator: (value) => {
+          return validator.isAlpha(value);
+        },
+        message: "First name must contain only alphabetical characters",
+      },
     },
     email: {
       type: String,
@@ -21,15 +35,38 @@ const userSchema = new mongoose.Schema(
       unique: true,
       lowercase: true,
       required: [true, "Email is required field"],
+      validate: {
+        validator: (value) => {
+          return validator.isEmail(value);
+        },
+        message: "Email is not valid: {VALUE}",
+      },
     },
-    password: { type: String, required: [true, "Password is required field"] },
+    password: {
+      type: String,
+      required: [true, "Password is required field"],
+      validate: [
+        {
+          validator: (value) => {
+            return value.length >= 8;
+          },
+          message: "Password length must be greater or equal to 8: {VALUE}",
+        },
+        {
+          validator: (value) => {
+            return validator.isStrongPassword(value);
+          },
+          message: "Password must be strong password: {VALUE}",
+        },
+      ],
+    },
     phone: {
       type: String,
       validate: {
         validator: (value) => {
-          return value.length === 10;
+          return validator.isMobilePhone(value, "en-IN");
         },
-        message: "Phone number must be 10 digits",
+        message: "Invalid Phone Number: {VALUE}",
       },
     },
     age: {
@@ -44,9 +81,44 @@ const userSchema = new mongoose.Schema(
         message: "`{VALUE}` value is not supported for `{PATH}` field",
       },
     },
-    photoURL: { type: String },
-    about: { type: String, default: "THis is defaultabout user text" },
-    skills: { type: [String] },
+    photoURL: {
+      type: String,
+      validate: {
+        validator: (value) => {
+          return validator.isURL(value);
+        },
+        message: "Invalid URL: {VALUE}",
+      },
+    },
+    about: {
+      type: String,
+      default: "This is default about user text",
+      maxLength: 200,
+      trim: true,
+      validate: {
+        validator: (value) => {
+          return validator.isAlpha(value.replace(/\s/g, ""));
+        },
+        message: "About field should contains alphabets value: {VALUE}",
+      },
+    },
+    skills: {
+      type: [String],
+      validate: [
+        {
+          validator: (value) => {
+            return value.every((skill) => validator.isAlpha(skill));
+          },
+          message: "Skills must contain only alphabetical characters",
+        },
+        {
+          validator: (value) => {
+            return value.length <= 10;
+          },
+          message: "Skills must contain at most 10 skills",
+        },
+      ],
+    },
   },
   { timestamps: true }
 );
