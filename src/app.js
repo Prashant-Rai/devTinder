ebbf9df84035a8d12dfaf3923e934da078828668
prashant-rai <prashantrai117@gmail.com>
2025-10-08 00:15:35 +0530

    Added CRUD APIS

--- src/app.js
+++ src/app.js
@@ -3,23 +3,82 @@ const { connectDB } = require("./config/database");
 const User = require("./models/user");
 
 const app = express();
+app.use(express.json());
 
 app.post("/signup", async (req, res) => {
-  const user = {
-    firstName: "Prashant",
-    lastName: "Rai",
-    email: "prash@rai.com",
-    password: "123456",
-    age: 32,
-    gender: "Male",
-  };
-
   try {
-    const newUser = new User(user);
+    const newUser = new User(req.body);
     await newUser.save();
     res.send("User signed up successfully");
   } catch (err) {
-    res.status(400).send("Error while creating the user", err.message);
+    res.status(400).send("Error while creating the user" + err.message);
+  }
+});
+
+app.get("/user", async (req, res) => {
+  try {
+    const users = await User.find({ email: req.query.email });
+    if (users.length === 0) {
+      res.status(404).send("No user found");
+    } else {
+      res.send(users);
+    }
+  } catch (err) {
+    res.status(400).send("Something went wrong" + err.message);
+  }
+});
+
+app.get("/feed", async (req, res) => {
+  try {
+    const users = await User.find({});
+    if (users.length === 0) {
+      res.status(404).send("No user found");
+    } else {
+      res.send(users);
+    }
+  } catch (err) {
+    res.status(400).send("Something went wrong" + err.message);
+  }
+});
+
+app.delete("/user", async (req, res) => {
+  try {
+    const id = req.query.id;
+    const users = await User.findByIdAndDelete(id);
+    res.send("User deleted successfully");
+  } catch (err) {
+    res.status(400).send("Something went wrong" + err.message);
+  }
+});
+
+app.patch("/user", async (req, res) => {
+  try {
+    const id = req.body.id;
+    const user = await User.findByIdAndUpdate(id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+    res
+      .status(200)
+      .send({ message: "User updated successfully", ...user._doc });
+  } catch (err) {
+    res.status(400).send("Something went wrong" + err.message);
+  }
+});
+
+app.put("/user", async (req, res) => {
+  try {
+    const id = req.body.id;
+    const user = await User.findByIdAndUpdate(id, req.body, {
+      new: true,
+      overwrite: true,
+      runValidators: true, //if any validation is added in schema it will run for update query as well by defaut it will run on insertion only
+    });
+    res
+      .status(200)
+      .send({ message: "User updated successfully", ...user._doc });
+  } catch (err) {
+    res.status(400).send("Something went wrong" + err.message);
   }
 });
 
